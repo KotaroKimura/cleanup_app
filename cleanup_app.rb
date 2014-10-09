0aa2f02fdc7b934fa30af04bd90449ce323745ec
kotaro_kimura <kotaro_k@icloud.com>
2014-10-09 23:50:21 +0900

    ランダムで掃除のアクションを自動生成する

--- cleanup_app.rb
+++ cleanup_app.rb
@@ -1,14 +1,33 @@
 #! ruby -Ku
 #encoding: utf-8
 
+###データベースのカラムについて###
+#・id: そのまま
+#・locations: 場所(居間、台所)
+#・action: 掃除を行うアクション(○○する)
+#・boolean: やったorやってない
+#・priority: 順序付け(使うかは未定)
+
+
+
 require 'rubygems'
 require 'sqlite3'
+require 'pry-byebug'
 
 DBNAME = "/vagrant/clean_up_app/cleanup.sqlite3"
-db = SQLite3::Database.new( DBNAME )
 
-db.execute("SELECT * FROM personal") do |row|
-	p row
+def main
+  db = SQLite3::Database.new(DBNAME)
+  db.execute( "SELECT * FROM cleanup ORDER BY RANDOM() limit 1;") do |record|
+  	binding.pry
+  	if record[3] == 0
+  	  db.execute("update cleanup set boolean = 1 where id = #{record[0]}")
+  	  return record[2]
+    else
+  	  main
+    end
+  end
+  db.close
 end
 
-db.close
+puts "本日の掃除は、　ヾ(*・ω・)ノ【" + main + "】ヾ(・ω・*)ノ　です！"
