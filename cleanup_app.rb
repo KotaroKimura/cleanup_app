0ed67d686aa56487a8d6373aaea763758c261480
kotaro_kimura <kotaro_k@icloud.com>
2014-10-10 22:37:20 +0900

    変数・関数・クラス名の最適化

--- cleanup_app.rb
+++ cleanup_app.rb
@@ -5,32 +5,24 @@ require 'rubygems'
 require 'sqlite3'
 require 'pry-byebug'
 
-class BooleanValue
+class CleanUp
   def check_boolean_value(boolean_value, record_num, all_record)
     for count in 1..record_num do
       boolean_value = boolean_value + all_record[count - 1][3]
     end
     boolean_value
   end
-end
 
-class SelectCleanupAction
-  def return_inital_values(db)
+  def restart_cleanup_action(db)
     db.execute("update cleanup set boolean = 0;")
   end
 
-  def random_select(db)
-    db.execute("SELECT * FROM cleanup ORDER BY RANDOM() limit 1;") do |one_record|
-      db.execute("update cleanup set boolean = 1 where id = #{one_record[0]};")
-    end
-  end
-
-  def select_unclean_area(db)
+  def select_cleanup_action(db)
     db.execute("SELECT * FROM cleanup where boolean = 0;").sample(1)
   end
 
-  def update_boolean_value(unclean_area, db)
-    db.execute("update cleanup set boolean = 1 where id = #{unclean_area[0]};")
+  def done_cleanup_action(select, db)
+    db.execute("update cleanup set boolean = 1 where id = #{select[0]};")
   end
 end
 
@@ -42,13 +34,9 @@ all_record = db.execute("SELECT * FROM cleanup;")
 record_num = db.execute("select count(*) from cleanup;")[0][0]
 boolean_value = 0
 
-if BooleanValue.new().check_boolean_value(boolean_value, record_num, all_record) == record_num
-  SelectCleanupAction.new().return_inital_values(db)
-  SelectCleanupAction.new().random_select(db)
-else
-  unclean_area = SelectCleanupAction.new().select_unclean_area(db)
-  update = SelectCleanupAction.new().update_boolean_value(unclean_area[0], db)
-end
+CleanUp.new().restart_cleanup_action(db) if CleanUp.new().check_boolean_value(boolean_value, record_num, all_record) == record_num
+select = CleanUp.new().select_cleanup_action(db)
+done = CleanUp.new().done_cleanup_action(select[0], db)
 
 db.close
 
