05072cab92805e4c3d734ce7efaba0c5c4c31ae4
kotaro_kimura <kotaro_k@icloud.com>
2014-10-10 21:45:51 +0900

    「boolean」の値がすべて「true」の時の処理

--- ReadMe.md
+++ ReadMe.md
@@ -2,5 +2,5 @@
 #・id: そのまま
 #・locations: 場所(居間、台所)
 #・action: 掃除を行うアクション(○○する)
-#・boolean: やったorやってない
+#・boolean: やった(true)orやってない(false) => (1 OR 0)
 #・priority: 順序付け(使うかは未定)
--- cleanup_app.rb
+++ cleanup_app.rb
@@ -5,6 +5,28 @@ require 'rubygems'
 require 'sqlite3'
 require 'pry-byebug'
 
+class BooleanValue
+  def check_boolean_value(boolean_value, record_num, all_record)
+    for count in 1..record_num do
+      boolean_value = boolean_value + all_record[count - 1][3]
+    end
+    boolean_value
+  end
+end
+
+class SelectCleanupAction
+  def return_inital_values(db)
+    db.execute("update cleanup set boolean = 0;")
+  end
+
+  def random_select(db)
+    db.execute("SELECT * FROM cleanup ORDER BY RANDOM() limit 1;") do |one_record|
+      db.execute("update cleanup set boolean = 1 where id = #{one_record[0]};")
+    end
+  end
+end
+
+
 DBPATH = "/vagrant/clean_up_app/cleanup.sqlite3"
 
 db = SQLite3::Database.new(DBPATH)
@@ -12,29 +34,14 @@ all_record = db.execute("SELECT * FROM cleanup;")
 record_num = db.execute("select count(*) from cleanup;")[0][0]
 boolean_value = 0
 
-for count in 1..record_num do
-  all_record[count - 1][3]
-  boolean_value = boolean_value + all_record[count - 1][3]
-end
-
-if boolean_value == record_num
-  puts "booleanの値がすべてtrue" 
+if BooleanValue.new().check_boolean_value(boolean_value, record_num, all_record) == record_num
+  SelectCleanupAction.new().return_inital_values(db)
+  SelectCleanupAction.new().random_select(db)
 else
   puts "booleanの値が一つでもfalse"
+  #booleanが「0」のレコードを取得し、ランダムで選択
 end
 
+db.close
 
-#def main
-  #db.execute("SELECT * FROM cleanup;") do |record|
-  #	binding.pry
-  #	if record[3] == 0
-  #	  db.execute("update cleanup set boolean = 1 where id = #{record[0]}")
-  #	  return record[2]
-  #  else
-  #	  main
-  #  end
-  #end
-  #db.close
-#end
 
-#puts "本日の掃除は、　ヾ(*・ω・)ノ【" + main + "】ヾ(・ω・*)ノ　です！"
