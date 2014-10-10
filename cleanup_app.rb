982bb75aaf4f55f45b317724ad51cefb74dcfe96
kotaro_kimura <kotaro_k@icloud.com>
2014-10-10 20:32:20 +0900

    カラム「boolean」の値を見て条件分岐できる

--- cleanup_app.rb
+++ cleanup_app.rb
@@ -1,33 +1,40 @@
 #! ruby -Ku
 #encoding: utf-8
 
-###データベースのカラムについて###
-#・id: そのまま
-#・locations: 場所(居間、台所)
-#・action: 掃除を行うアクション(○○する)
-#・boolean: やったorやってない
-#・priority: 順序付け(使うかは未定)
-
-
-
 require 'rubygems'
 require 'sqlite3'
 require 'pry-byebug'
 
-DBNAME = "/vagrant/clean_up_app/cleanup.sqlite3"
+DBPATH = "/vagrant/clean_up_app/cleanup.sqlite3"
 
-def main
-  db = SQLite3::Database.new(DBNAME)
-  db.execute( "SELECT * FROM cleanup ORDER BY RANDOM() limit 1;") do |record|
-  	binding.pry
-  	if record[3] == 0
-  	  db.execute("update cleanup set boolean = 1 where id = #{record[0]}")
-  	  return record[2]
-    else
-  	  main
-    end
-  end
-  db.close
+db = SQLite3::Database.new(DBPATH)
+all_record = db.execute("SELECT * FROM cleanup;")
+record_num = db.execute("select count(*) from cleanup;")[0][0]
+boolean_value = 0
+
+for count in 1..record_num do
+  all_record[count - 1][3]
+  boolean_value = boolean_value + all_record[count - 1][3]
 end
 
-puts "本日の掃除は、　ヾ(*・ω・)ノ【" + main + "】ヾ(・ω・*)ノ　です！"
+if boolean_value == record_num
+  puts "booleanの値がすべてtrue" 
+else
+  puts "booleanの値が一つでもfalse"
+end
+
+
+#def main
+  #db.execute("SELECT * FROM cleanup;") do |record|
+  #	binding.pry
+  #	if record[3] == 0
+  #	  db.execute("update cleanup set boolean = 1 where id = #{record[0]}")
+  #	  return record[2]
+  #  else
+  #	  main
+  #  end
+  #end
+  #db.close
+#end
+
+#puts "本日の掃除は、　ヾ(*・ω・)ノ【" + main + "】ヾ(・ω・*)ノ　です！"
